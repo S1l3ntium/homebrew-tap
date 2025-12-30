3a9d0942c6f408fc13baf7f5d0ef89caf798c580
Denis Chichmarev <35502557+S1l3ntium@users.noreply.github.com>
2025-12-30 15:27:57 +0300

    Update homebrew-services-manager.rb

--- Casks/homebrew-services-manager.rb
+++ Casks/homebrew-services-manager.rb
@@ -1,1 +1,16 @@
+cask 'homebrew-services-manager' do
+  version '1.0.0'
+  sha256 '9e62be93d759b30859dbc1eb40ea4571623fd04121d29e8900f6b53041926247'
 
+  url "https://github.com/S1lentium/homebrew-services-manager/releases/download/v#{version}/HomebrewServicesManager.app.zip"
+  name 'Homebrew Services Manager'
+  desc 'A lightweight menu bar application for managing Homebrew services on macOS'
+  homepage 'https://github.com/S1l3ntium/homebrew-services-manager'
+
+  app 'HomebrewServicesManager.app'
+
+  zap trash: [
+    '~/Library/Preferences/com.homebrew.services-manager.plist',
+    '~/Library/Caches/com.homebrew.services-manager'
+  ]
+end
